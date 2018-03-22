5130040d8764288f3b77c35bed4f202bf56bfce5
Subodh <subodheenitr@gmail.com>
2018-03-22 02:59:09 -0500

    Added Launch file to launch stereo node

--- Examples/ROS/ORB_SLAM2/src/ros_stereo.cc
+++ Examples/ROS/ORB_SLAM2/src/ros_stereo.cc
@@ -56,6 +56,7 @@ int main(int argc, char **argv)
     if(argc != 4)
     {
         cerr << endl << "Usage: rosrun ORB_SLAM2 Stereo path_to_vocabulary path_to_settings do_rectify" << endl;
+        cerr << "No of arguments received is " << argc <<endl;
         ros::shutdown();
         return 1;
     }    
@@ -110,7 +111,7 @@ int main(int argc, char **argv)
     ros::NodeHandle nh;
 
     message_filters::Subscriber<sensor_msgs::Image> left_sub(nh, "/camera/left/image_raw", 1);
-    message_filters::Subscriber<sensor_msgs::Image> right_sub(nh, "camera/right/image_raw", 1);
+    message_filters::Subscriber<sensor_msgs::Image> right_sub(nh, "/camera/right/image_raw", 1);
     typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::Image, sensor_msgs::Image> sync_pol;
     message_filters::Synchronizer<sync_pol> sync(sync_pol(10), left_sub,right_sub);
     sync.registerCallback(boost::bind(&ImageGrabber::GrabStereo,&igb,_1,_2));
